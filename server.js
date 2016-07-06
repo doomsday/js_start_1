4ebb2fbf38749e86b08f27c0367205f7011d438f
Mikhail Gashenko <dr.psycho@live.com>
2016-07-06 10:29:42 +0600

    getOwnPropertyDescriptor

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+function inherit (p) {
+  if (p == null) throw TypeError()
+  if (Object.create) return Object.create(p)
+  var t = typeof p
+  if (t !== 'object' && t !== 'function') throw TypeError()
+  function F () { };
+  F.prototype = p
+  return new F()
+}
 
-app.get('/listUsers', function (request, response) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       response.end( data );
-   });
-});
+var o1 = {
+  data_prop: 111,
+  get accessor_prop () { return this.data_prop },
+  set accessor_prop (value) { this.data_prop = value }
+}
 
-var server = app.listen(8081, function () {
+var q = inherit(o1)
+q.x = 1
+q.y = 1
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("Example app listening at http://%s:%s", host, port);
-
-});
-
-/*
-var http = require("http");
-
-http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
-*/
+try {
+  console.log(Object.getOwnPropertyDescriptor(o1, 'data_prop'))
+  console.log(Object.getOwnPropertyDescriptor(o1, 'accessor_prop'))
+} catch (err) {
+  console.log('exception: ' + err.message + ', exception type: ' + err.name)
+}
